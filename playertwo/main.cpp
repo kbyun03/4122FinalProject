31f5938290f7527cab72d415bfb75444370944a8
Jackie Kang <jackiekang@lawn-128-61-1-38.lawn.gatech.edu>
2018-04-30 15:53:12 -0400

    user ip address display

--- playertwo/main.cpp
+++ playertwo/main.cpp
@@ -8,14 +8,22 @@ Game * game;
 
 int main(int argc, char *argv[])
 {
-    // show ip address
-    foreach (const QHostAddress &address, QNetworkInterface::allAddresses()) {
-        if (address.protocol() == QAbstractSocket::IPv4Protocol && address != QHostAddress(QHostAddress::LocalHost))
-             qDebug() << address.toString();
-    }
 
     QApplication a(argc, argv);
     // we could set window icon
+
+    // show ip address
+    QGraphicsScene * addrScene = new QGraphicsScene();
+    addrScene->setSceneRect(0,0,480,140);
+    QGraphicsTextItem * addrText = new QGraphicsTextItem;
+    addrText->setPos(280,30);
+    foreach (const QHostAddress &address, QNetworkInterface::allAddresses()) {
+        if (address.protocol() == QAbstractSocket::IPv4Protocol && address != QHostAddress(QHostAddress::LocalHost))
+            addrText->setPlainText("Your IP address:\n\n\t" + address.toString());
+    }
+    addrScene->addItem(addrText);
+    QGraphicsView * view = new QGraphicsView(addrScene);
+    view->show();
 
     QSplashScreen *splash = new QSplashScreen;
     splash->setPixmap(QPixmap(":/images/Title.png"));
